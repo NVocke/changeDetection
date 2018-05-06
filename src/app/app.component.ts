840aebefa74957865a64231d1fab39cb805e6ffc
Niels Vocke <n.vocke@gmx.de>
2018-05-06 14:01:16 +0200

    Simple Implementation of OnPush-Detection with ngOnChanges.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  mainStatus = { changed: false };
+
+  constructor() {
+    setTimeout(() => {
+      this.mainStatus = { changed: true };
+    }, 2000);
+  }
 }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,26 @@
+import { Component, ChangeDetectionStrategy, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+  // OnPush only works when state changes through @Input() injection.
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainComponent implements OnChanges {
+
+  @Input() status;
+  counter = 0;
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.status = { changed: true };
+    // }, 2000);
+  }
+
+  ngOnChanges() {
+    console.log('Changes: ', this.counter);
+    this.counter++;
+  }
+
+}
